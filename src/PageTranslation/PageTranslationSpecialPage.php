77753f737eaa832ffdc3fab0bde6cab7101ba5f3
nmlgc <nmlgc@nmlgc.net>
2022-12-20 12:43:53 +0100

    [thpatch] Remove the priority language section from Special:PageTranslation
    We'll be running a fork of this extension for the foreseeable future
    anyway. It's more robust to remove this section from PHP rather than
    hiding it via a CSS rule that can easily get outdated whenever upstream
    adds new sections to the page (which did in fact happen between 2014
    and now). In PHP, we'd at least get a merge conflict in that case.
    
    Change-Id: I85c79d98462be43479611c417ab5763e1ef107b4

--- src/PageTranslation/PageTranslationSpecialPage.php
+++ src/PageTranslation/PageTranslationSpecialPage.php
@@ -955,8 +955,6 @@ class PageTranslationSpecialPage extends SpecialPage {
 			$out->wrapWikiMsg( Html::successBox( '$1' ), 'tpt-mark-nochanges' );
 		}
 
-		$this->priorityLanguagesForm( $page );
-
 		// If an existing page does not have the supportsTransclusion flag, keep the checkbox unchecked,
 		// If the page is being marked for translation for the first time, the checkbox can be checked
 		$this->templateTransclusionForm( $page->supportsTransclusion() ?? $firstMark );
